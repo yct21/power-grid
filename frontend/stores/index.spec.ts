695e4b00e5809e44a142dff16097b8694049808a
Chutian Yang <yct21@12tcy.com>
2017-07-04 14:21:03 +0800

    Write stores/index

--- frontend/stores/index.spec.ts
+++ frontend/stores/index.spec.ts
@@ -1,11 +1,25 @@
-// import { createStore } from "stores";
+import * as td from "testdouble";
+import { assert } from "chai";
+import { TestScheduler } from "rxjs/testing/TestScheduler";
 
-// describe("frontend/stores", () => {
-//   test("Scenerio: create store when application starts with `currentGameId` don't exist", () => {
-//     // given
+describe("frontend/stores", () => {
+  it("could create a temporary store at start up", () => {
+    // Given we can create a StartupTempStore
+    const fakeCreateStartupTempStore = td.function("fakeCreateStartupTempStore");
+    const fakeStore = "fakeStore";
+    td.when(fakeCreateStartupTempStore()).thenReturn(fakeStore);
+    td.replace("stores/startupTempStore", { createStartupTempStore: fakeCreateStartupTempStore });
 
-//     // when
-//     // const store = createStore();
-//     // expect
-//   });
-// });
+    // And DOMSource is provided
+    const fakeDomSource = "fakeDomSource";
+
+    // When createStore is called
+    const { createStore } = require("./index");
+    const store$ = createStore(fakeDomSource);
+
+    // Then it returns a StartupTempStore
+    const scheduler = new TestScheduler(assert.deepEqual);
+    scheduler.expectObservable(store$).toBe("x-", {x: fakeStore});
+    scheduler.flush();
+  });
+});
--- frontend/stores/index.ts
+++ frontend/stores/index.ts
@@ -1,7 +1,11 @@
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import "rxjs/add/observable/of";
+import { DOMSource } from "@cycle/dom/rxjs-typings";
 import { StartupTempStore, createStartupTempStore } from "stores/startupTempStore";
 
 export type Store = StartupTempStore;
 
-export function createStore(): Store {
-  return createStartupTempStore();
+export function createStore(domSource: DOMSource): BehaviorSubject<Store> {
+  const store = createStartupTempStore();
+  return new BehaviorSubject(store);
 }
