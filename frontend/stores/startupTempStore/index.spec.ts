294eadf354db748e3f2febe1e5f6c1582c89d19c
Chutian Yang <yct21@12tcy.com>
2017-07-07 19:45:16 +0800

    Change startupTempStore to be a plain object

--- frontend/stores/startupTempStore/index.spec.ts
+++ frontend/stores/startupTempStore/index.spec.ts
@@ -4,9 +4,9 @@ describe("stores/startupTempstore/index", () => {
   it("could create a temporary store when application starts", () => {
     // When creating startupTempStore
     const { createStartupTempStore } = require("./index");
-    const store$ = createStartupTempStore();
+    const store = createStartupTempStore();
 
     // Then it should create a BehaviorSubject with simple plain value
-    assert.deepEqual(store$.getValue(), { storeName: "StartupTempStore" });
+    assert.deepEqual(store, { storeName: "StartupTempStore" });
   });
 });
--- frontend/stores/startupTempStore/index.ts
+++ frontend/stores/startupTempStore/index.ts
@@ -1,12 +1,10 @@
-import { BehaviorSubject } from "rxjs/BehaviorSubject";
-
 // We are only expecting connected event when application started.
 export interface StartupTempStore {
   storeName: "StartupTempStore",
 }
 
-export function createStartupTempStore(): BehaviorSubject<StartupTempStore> {
+export function createStartupTempStore(): StartupTempStore {
   const initialValue: StartupTempStore = { storeName: "StartupTempStore" };
 
-  return new BehaviorSubject(initialValue);
+  return initialValue;
 }
