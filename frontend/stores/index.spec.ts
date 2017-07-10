853a9e421478e63781359e2162efacf806339458
Chutian Yang <yct21@12tcy.com>
2017-07-10 10:37:39 +0800

    Write some part of store/index

--- frontend/stores/index.spec.ts
+++ frontend/stores/index.spec.ts
@@ -2,24 +2,79 @@ import * as td from "testdouble";
 import { assert } from "chai";
 import { TestScheduler } from "rxjs/testing/TestScheduler";
 
-describe("frontend/stores", () => {
-  it("could create a temporary store at start up", () => {
-    // Given we can create a StartupTempStore
-    const fakeCreateStartupTempStore = td.function("fakeCreateStartupTempStore");
-    const fakeStore = "fakeStore";
-    td.when(fakeCreateStartupTempStore()).thenReturn(fakeStore);
-    td.replace("stores/startupTempStore", { createStartupTempStore: fakeCreateStartupTempStore });
+function mockCreateStartupTempStore() {
+  const fakeCreateStartupTempStore = td.function("fakeCreateStartupTempStore");
+  const fakeStore = "fakeStore";
+  td.when(fakeCreateStartupTempStore()).thenReturn(fakeStore);
+  td.replace("stores/startupTempStore", { createStartupTempStore: fakeCreateStartupTempStore });
 
-    // And DOMSource is provided
-    const fakeDomSource = "fakeDomSource";
+  return fakeCreateStartupTempStore;
+}
 
-    // When createStore is called
-    const { createStore } = require("./index");
-    const store$ = createStore(fakeDomSource);
+interface EventMarbles {
+  connectSocketMarble: string,
+  enterGameMarble: string,
+  exitGameMarble: string,
+}
 
-    // Then it returns a StartupTempStore
-    const scheduler = new TestScheduler(assert.deepEqual);
-    scheduler.expectObservable(store$).toBe("x-", {x: fakeStore});
-    scheduler.flush();
+function mockIntent(
+  scheduler: TestScheduler,
+  eventMarbles: EventMarbles,
+) {
+  const { connectSocketMarble, enterGameMarble, exitGameMarble } = eventMarbles;
+  const connectSocket$ = scheduler.createColdObservable(
+    connectSocketMarble,
+    { a: { eventName: "connect_socket" }},
+  );
+
+  const enterGameSocket$ = scheduler.createColdObservable(
+    enterGameMarble,
+    { b: { eventName: "enter_game" } },
+  );
+
+  const exitGameSocket$ = scheduler.createColdObservable(
+    exitGameMarble,
+    { c: { eventName: "exit_game" } },
+  )
+
+  const fakeIntent = td.function();
+  td.when(fakeIntent("fakeDomSource", "fakeSocket")).thenReturn({
+    connectSocket$,
+    enterGameSocket$,
+    exitGameSocket$
   });
+
+  td.replace("stores/intent", { intent: fakeIntent });
+}
+
+function expectStore(scheduler: TestScheduler, store: Store, marble: string) {
+  scheduler.expectObservable()
+}
+
+describe("frontend/stores", () => {
+  describe("Feature: create store stream as the model of application", () => {
+    it("Scenario: store switches normally", () => {
+      // Given we can create a StartupTempStore
+      mockCreateStartupTempStore();
+
+      // And DOMSource is provided
+      const fakeDomSource = "fakeDomSource";
+
+      // And we have streams for switching store
+      const scheduler = new TestScheduler(assert.deepEqual);
+      mockIntent(scheduler, {
+        connectSocketMarble: "-x^",
+        enterGameMarble:     "--x-",
+        exitGameMarble:      "---x--",
+      })
+
+      // When createStore is called
+      const { createStore } = require("./index");
+      const store$ = createStore(fakeDomSource);
+
+      // Then it returns a StartupTempStore
+      scheduler.expectObservable(store$).toBe("x-", {x: fakeStore});
+      scheduler.flush();
+    });
+  })
 });
--- frontend/stores/index.ts
+++ frontend/stores/index.ts
@@ -1,11 +1,82 @@
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/merge";
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/switchMap";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
-import "rxjs/add/observable/of";
 import { DOMSource } from "@cycle/dom/rxjs-typings";
 import { StartupTempStore, createStartupTempStore } from "stores/startupTempStore";
+import { LandingPageStore, createLandingPageStore } from "stores/landingPageStore";
+import { GamePageStore, createGamePageStore } from "stores/gamePageStore";
+import { getSocket } from "socket";
+import { SwitchStoreEvents, intent } from "stores/intent";
 
-export type Store = StartupTempStore;
+export type Store = StartupTempStore | LandingPageStore | GamePageStore;
 
-export function createStore(domSource: DOMSource): BehaviorSubject<Store> {
-  const store = createStartupTempStore();
-  return new BehaviorSubject(store);
+/*
+     +--------------------+
+     |  StartupTempStore  |
+     +--------------------+
+                |
+                | Connect socket
+                v
+     +--------------------+
+     |  LandingPageStore  |
+     +------+-------------+
+            |        ^
+Enter game  |        |  Exit game
+            v        |
+     +--------------------+
+     |   GamePageStore    |
+     +--------------------+
+  */
+
+function legalSwitchStore(store$: BehaviorSubject<Store>) {
+  return (switchStoreEvent: SwitchStoreEvents) => {
+    const store = store$.getValue();
+
+    if (switchStoreEvent.event === "connect_socket") {
+      return store.storeName === "StartupTempStore";
+    } else if (switchStoreEvent.event === "enter_game") {
+      return store.storeName === "LandingPageStore";
+    } else if (switchStoreEvent.event === "exit_game") {
+      return store.storeName === "GamePageStore";
+    }
+
+    return false;
+  }
+}
+
+export function createStore(domSource: DOMSource, debounceTime: number = 2000): BehaviorSubject<Store> {
+  // This is the internal subscription of store
+  const initialValue: Store = createStartupTempStore();
+  const store$: BehaviorSubject<Store> = new BehaviorSubject(initialValue);
+
+  const socket = getSocket();
+  const {
+    connectSocket$,
+    enterGame$,
+    exitGame$,
+  } = intent(domSource, socket);
+
+  const switchStore$ = connectSocket$
+    .merge(connectSocket$)
+    .merge(enterGame$)
+    .merge(exitGame$)
+    .debounceTime(debounceTime)
+    .filter(legalSwitchStore(store$));
+
+  switchStore$.switchMap((switchStoreEvent: SwitchStoreEvents): Observable<Store> => {
+    if (switchStoreEvent.event === "connect_socket") {
+      return createLandingPageStore(domSource, socket);
+    } else if (switchStoreEvent.event === "enter_game") {
+      return createGamePageStore(domSource, socket);
+    } else if (switchStoreEvent.event === "exit_game") {
+      return createLandingPageStore(domSource, socket);
+    }
+
+    throw new Error("Error in SwitchStoreEvent");
+  }).subscribe(store$);
+
+  return store$;
 }
