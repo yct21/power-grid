267cee320836e09adf995d2d395967eda1f13a51
Chutian Yang <yct21@12tcy.com>
2017-07-13 09:06:05 +0800

    Test socket.io in backend to see if it works

---
+++ backend/index.ts
@@ -0,0 +1,22 @@
+import * as express from "express";
+import * as http from "http";
+import * as socketIO from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+server.listen(8002);
+const io = socketIO(server);
+
+app.use((req: any, res: any, next: any) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8001');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+io.on('connection', function (socket) {
+  console.log("test")
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
