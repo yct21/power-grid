b0ee0fe23edc2e69ad9f4e178ae28c4c8a6051c4
Chutian Yang <yct21@12tcy.com>
2017-08-07 20:13:27 +0800

    Change the main frame to tradition box model

--- frontend/entry/MainComponent/index.ts
+++ frontend/entry/MainComponent/index.ts
@@ -7,7 +7,6 @@ import { InitialParams as LandingPageInitialParameters } from "LandingPage/store
 // This is actully a router
 export default function MainComponent({DOM: domSource}: {DOM: DOMSource}) {
   const view$ = switchPage$().switchMap((parameter: LandingPageInitialParameters) => {
-    console.log(parameter)
     const page = LandingPage({ storeInitialParameters: parameter, DOM: domSource });
 
     return page.DOM;
