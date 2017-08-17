aaa9a2d670e40feb8f499592b751b6560f92d8ce
Chutian Yang <yct21@12tcy.com>
2017-08-17 11:59:29 +0800

    Write spec for LandingPage/store/index

--- frontend/LandingPage/store/__specs__/index.spec.ts
+++ frontend/LandingPage/store/__specs__/index.spec.ts
@@ -1,50 +1,42 @@
-import { TestScheduler } from "rxjs/Testing/TestScheduler";
 import { assert } from "chai";
 import * as td from "testdouble";
-const random = require("lodash/random");
 
 describe("LandingPage/store", () => {
   it("Feature: it could create store with proper parameter", () => {
-    const scheduler = new TestScheduler(assert.deepEqual);
-
-    // Given store has onlineNum as initial parameter
+    // Given we could get initial parameter from caller
+    const fakeDom = "fakeDom";
     const initialParams = {
-      onlineNum: 1024,
+      onlineNum: 42,
     };
 
-    // And a stream of refreshOnlineNum
-    const onlineNum = random();
-    const refreshOnlineNum$ = scheduler.createColdObservable("----x--x--", { x: onlineNum });
-    td.replace("socket/refreshOnlineNum", { refreshOnlineNum$: () => refreshOnlineNum$ });
+    // And we could create onlineNum$ in store/onlineNum
+    const fakeOnlineNum$ = "fakeOnlineNum$";
+    const onlineNum = td.function();
+    td.when(onlineNum(initialParams.onlineNum)).thenReturn(fakeOnlineNum$);
+    td.replace("LandingPage/store/onlineNum", { onlineNum });
 
-    // And a stream of networkStatus
-    const networkStatus$ = scheduler.createColdObservable("----x---x----");
-    td.replace("socket/networkStatus", { networkStatus$: () => networkStatus$ } );
+    // And we could create networkStatus$ in store/networkStatus
+    const fakeNetworkStatus$ = "fakeNetworkStatus$";
+    const networkStatus = td.function();
+    td.when(networkStatus()).thenReturn(fakeNetworkStatus$);
+    td.replace("LandingPage/store/networkStatus", { networkStatus });
 
-    // And a stream of userName and event changeUserName
+    // And we could create UserName module in store/userName
     const fakeUserName$ = "fakeUserName";
-    const fakeChangeUserName$ = "fakeChangeUserName";
-    td.replace("LandingPage/store/userName", {
-      userName: () => ({
-        userName$: fakeUserName$,
-        changeUserName$: fakeChangeUserName$,
-      }),
-    });
+    const userName = td.function();
+    td.when(userName(fakeDom)).thenReturn(fakeUserName$);
+    td.replace("LandingPage/store/userName", { userName });
 
-    // When create store
-    const store = require("../index").createStore(initialParams);
+    // When creating store
+    const store = require("../index").createStore(initialParams, fakeDom);
 
-    // Then the store has a stream of onlineNum
-    scheduler.expectObservable(store.props.onlineNum$).toBe("y---x--x--", { x: onlineNum, y: 1024 });
+    // Then it returns a store consists of onlineNum$
+    assert.equal(store.onlineNum$, fakeOnlineNum$);
 
-    // And the store has a stream of networkStatus
-    scheduler.expectObservable(store.props.networkStatus$).toBe("----x---x----");
+    // And networkStatus$
+    assert.equal(store.networkStatus$, fakeNetworkStatus$);
 
-    // And the store has a stream of userName as property and a stream of
-    // changeUserName as event
-    assert.equal(store.props.userName$, fakeUserName$);
-    assert.equal(store.events.changeUserName$, fakeChangeUserName$);
-
-    scheduler.flush();
+    // And userName
+    assert.equal(store.userName, fakeUserName$);
   });
 });
