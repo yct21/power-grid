2a49762b5dc9b88082d41d760af85846cfec35c8
Chutian Yang <yct21@12tcy.com>
2017-12-01 15:47:30 +0800

    Add rx and vue-rx into dependency

--- client/entry/index.ts
+++ client/entry/index.ts
@@ -1,6 +1,16 @@
 import Vue from 'vue'
+import VueRx from 'vue-rx'
+import { Observable } from 'rxjs/Observable'
+import { Subscription } from 'rxjs/Subscription'
+import { Subject } from 'rxjs/Subject'
 import 'entry/global.css'
 import App from '../App/index.vue'
+
+Vue.use(VueRx, {
+  Observable,
+  Subscription,
+  Subject,
+})
 
 new Vue({
   el: '#app',
