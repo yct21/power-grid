4fb847992c5a1371042a290465ae39f0980f334f
Chutian Yang <yct21@12tcy.com>
2017-12-04 10:35:52 +0800

    Implement client/entry/loadParameters.ts

--- client/entry/loadParameters.ts
+++ client/entry/loadParameters.ts
@@ -1,11 +1,51 @@
-const userId = window.
+import { Observable } from 'rxjs/Observable'
+import { of as toObservable } from 'rxjs/Observable/of'
+import generate from 'nanoid/generate'
 
-const serverSocket = process.env.serverSocket
+interface Parameters {
+  // user data from local storage or url query
+  userId: string,
+  currentGameId: string | null,
 
-function getAppParameters () {
-
+  // websocket url from webpack define plugin
+  socketUrl: string,
 }
 
-export {
-  serverSocket,
+// pirated from
+// https://gomakethings.com/how-to-get-the-value-of-a-querystring-with-native-javascript/
+function getQueryString (field: string) {
+    var href = window.location.href;
+    var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+};
+
+function generateUserId (): string {
+  const allowSymbols =
+    "0123456789" +
+    "abcdefghijklmnopqrstuvwxyz" +
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  const suffixLength = 21
+  const userId = `User-${generate(allowSymbols, suffixLength)}`
+
+  return userId
+}
+
+export function loadParameters (): Observable<Parameters> {
+  const userId =
+    getQueryString('userId') ||
+    localStorage.getItem('userId') ||
+    generateUserId()
+
+  const currentGameId = localStorage.getItem('currentGameId')
+  const socketUrl = process.env.socketUrl as string
+
+  const parameters: Parameters = {
+    userId,
+    currentGameId,
+    socketUrl,
+  }
+
+  return toObservable(parameters)
 }
